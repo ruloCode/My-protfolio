4c892c908e61e5ef1b31f6645affc2103d00de8c
Andrés Santana <drew.lizcano@gmail.com>
2020-06-02 18:35:44 -0500

    Added font icons and social media

--- src/components/Provider/Provider.jsx
+++ src/components/Provider/Provider.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import './Provider.scss'
-import Shape1 from '../../assets/shape1.png'
-import Shape2 from '../../assets/shape2.png'
-import GitHub from '../../assets/github.png'
-import Linkedin from '../../assets/link.svg'
+import React from 'react';
+import './Provider.scss';
+import Shape1 from '../../assets/shape1.png';
+import Shape2 from '../../assets/shape2.png';
 
-const Provider = ({children})=>{
+const Provider = ({ children }) => {
   return (
     <div className='provider'>
-    <div className='social-media'>
-      <img className='social-icon' src={GitHub} alt='shapeImage' />
-      <i class="fab fa-linkedin-in"></i>
-    </div>
+      <div className='social-media'>
+        <a rel='noopener noreferrer' target='_blank' href='https://github.com/rulo-code'>
+          <i className='fab fa-github' />
+        </a>
+        <a rel='noopener noreferrer' target='_blank' href='https://www.linkedin.com/in/andr%C3%A9s-santana-lizcano-a4b960191/'>
+          <i className='fab fa-linkedin-in' />
+
+        </a>
+        <a rel='noopener noreferrer' target='_blank' href='https://twitter.com/rulo_code'>
+          <i className='fab fa-twitter' />
+        </a>
+      </div>
       <img className='shape top-corner' src={Shape2} alt='shapeImage' />
-      <img className='shape bottom-corner'src={Shape1} alt='shapeImage' />
+      <img className='shape bottom-corner' src={Shape1} alt='shapeImage' />
       {children}
     </div>
-  )
-}
+  );
+};
 
 export default Provider;
